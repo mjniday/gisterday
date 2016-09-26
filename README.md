8d2561215d999dd1b921d430d9b5e67f3090ad12
Michael Niday <mjniday@Michaels-MacBook-Pro.local>
2016-09-25 23:14:53 -0400

    Created methods to build gists from cli text editor

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 Usage
-* Use `gist [filename]` in order to create a new anonymoust gist. Currently this only works for files in your working directory.
+* Use `gist [filename]` in order to create a new anonymoust gist. 
+* If the file exists, it will be pushed to a new Gist. 
+* If the file does not exist, one can be created using Vim
 
 Options
 * Use the flag `-d` to set a description for the gist.
-* Use the flag `-v` for a more verbose http response.
+* Use the flag `-v` for a more verbose http response with the full response body.
 
 Still to come
-* A user can use `gist` without arguments to create a new gist in a text editor.
 * A user can authenticate to create non-anonymous gists.
-* A user can search their own gists.
+* A user can search and edit their existing gists.
---
+++ lib/gist.rb
@@ -0,0 +1,94 @@
+#!/usr/bin/env ruby
+require "optparse"
+require "net/http"
+require "json"
+
+def read_file(file)
+	File.read(file)
+end
+
+def create_gist(filename,gist_content,options)
+	payload = {
+	  'description' => options[:description] || "Gist created using Gisterday",
+	  'public' => true,
+	  'files' => {
+	    filename => {
+	      'content' => gist_content
+	    }
+	  }
+	}
+
+	uri = URI("https://api.github.com/gists")
+	req = Net::HTTP::Post.new(uri.path)
+	req.body = payload.to_json
+
+	# GitHub API is strictly via HTTPS, so SSL is mandatory
+	res = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => true) do |http|
+	  http.request(req)
+	end
+
+	format_response(res)
+end
+
+def format_response(response)
+	STDOUT.puts response.inspect
+	STDOUT.puts ""
+	STDOUT.puts response.body.inspect if options[:verbose]
+	STDOUT.puts ""
+	STDOUT.puts ""
+	STDOUT.puts "New Gist created at:"
+	STDOUT.puts JSON.parse(response.body)['html_url']
+	STDOUT.puts ""
+end
+
+options = {}
+
+opt_parser = OptionParser.new do |opt|
+  opt.banner = "Usage: gist [FILE] [OPTIONS]"
+  opt.separator  ""
+  opt.separator  "Commands"
+  opt.separator  "     <blank>: create a new gist in text editor"
+  opt.separator  "     <file>:  create a new gist using the specified file"
+  opt.separator  "     login:   log in to github account"
+  opt.separator  ""
+  opt.separator  "Options"
+  # opt.separator  "     -lns: choose which lines in the file should be included in gist"
+  opt.separator  "     -d:   set the description of the gist"
+  opt.separator  "     -v:   view full http response"
+
+  opt.on("-d","--description DESCRIPTION","set the gist description") do |description|
+    options[:description] = description
+  end
+
+  opt.on("-v","--verbose","view full response of the http request") do
+    options[:verbose] = true
+  end
+
+  opt.on("-h","--help","help") do
+    puts opt_parser
+  end
+end
+
+opt_parser.parse!
+
+if Dir.entries(".").include? ARGV[0]
+	contents = read_file(ARGV[0])
+	create_gist(ARGV[0],contents,options)
+else
+	case ARGV[0]
+		when "new"
+			puts "TODO: creating a new gist..."
+		when "help"
+		  puts opt_parser
+		else
+			file = ARGV[0]
+			system %Q(vim +"put ='# When you save this file, a gist will be created.'" #{file})
+
+			# check if user saved and create gist if so
+			if Dir.entries(".").include? file
+				contents = read_file(file)
+				create_gist(file,contents,options)
+			end
+	end
+end
+
